ee89a86a4db36eac3cd77497431c610fa1e12b71
Julien Dargelos <julien.dargelos@me.com>
2017-09-19 14:37:18 +0200

    Integration wip

---
+++ app/models/concerns/slugable.rb
@@ -0,0 +1,69 @@
+module Slugable
+	extend ActiveSupport::Concern
+
+	SLUG_ATTRIBUTE = :slug
+	SLUG_CHARS = (0..9).to_a + ('a'..'z').to_a
+  SLUG_LENGTH = 10
+
+  module ClassMethods
+
+		def slugs
+			@slugs ||= {}
+		end
+
+		protected
+
+		def has_slug attribute = nil, method = nil, length: nil, chars: nil, &block
+			attribute = attribute.present? ? attribute.try(:to_s).try(:to_sym) : SLUG_ATTRIBUTE
+
+			raise "Column \"#{attribute}\" doesn't exist in #{self}" unless attribute_names.include? attribute.to_s
+
+			unless method.is_a?(Proc) || block_given?
+				length ||= SLUG_LENGTH
+				chars ||= SLUG_CHARS
+				chars = chars.to_s if chars.is_a? Symbol
+				chars = chars.split if chars.is_a? String
+
+				method = -> { (0..length - 1).map{ chars.sample }.join }
+			else
+				method = block unless method.is_a? Proc
+			end
+
+			validates attribute, presence: true, uniqueness: true
+
+			self.slugs[attribute] = method
+		end
+	end
+
+  included do
+
+  	before_validation :set_slugs
+
+  	protected
+
+  	def set_slugs
+  		self.class.slugs.each { |attribute, _| set_slug attribute unless valid_slug? attribute }
+  	end
+
+  	def set_slug attribute = SLUG_ATTRIBUTE
+  		attribute = attribute.to_s.to_sym
+  		slug = self.class.slugs[attribute]
+  		send "#{attribute}=", nil
+
+  		if slug.is_a? Proc
+	    	send "#{attribute}=", slug.call until valid_slug? attribute
+	    	true
+	    else
+	    	false
+	    end
+	  end
+
+	  def unique_slug? attribute = SLUG_ATTRIBUTE
+	  	!(self.class.find_by(attribute => send(attribute)).try(:id) != id && id.present?)
+	  end
+
+	  def valid_slug? attribute = SLUG_ATTRIBUTE
+	  	send(attribute).present? && unique_slug?(attribute)
+	  end
+  end
+end
--- app/models/tracklist.rb
+++ app/models/tracklist.rb
@@ -1,23 +1,6 @@
 class Tracklist < ApplicationRecord
-  SLUG_CHARS = (0..9).to_a + ('a'..'z').to_a
-  SLUG_LENGTH = 10
+  include Slugable
 
-  after_initialize :set_uuid, :set_slug
-
-  validates :uuid, presence: true, uniqueness: true
-  validates :slug, presence: true, uniqueness: true
-
-  protected
-
-  def self.slug
-    (0..SLUG_LENGTH - 1).map{ SLUG_CHARS.sample }.join
-  end
-
-  def set_slug
-    self.slug = self.class.slug
-  end
-
-  def set_uuid
-    self.uuid = SecureRandom.uuid
-  end
+  has_slug :slug
+  has_slug :uuid, -> { SecureRandom.uuid }
 end
